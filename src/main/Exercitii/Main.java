8100301fd51bd59c9e68966dff19aa231cf50718
SilentWatcher45 <ovidiu.adrian92@gmail.com>
2020-10-31 20:27:40 +0200

    I forgot to save
    I m so stupid, sorry :(

--- src/main/Exercitii/Main.java
+++ src/main/Exercitii/Main.java
@@ -2,9 +2,9 @@ package main.Exercitii;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		Ex1 nr = new Ex1();
-		Vectori cifre;// = new Vectori();
+		Vectori cifre = null;// = new Vectori();
 		
 		int [] cif = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 		int [] cif2 = { 23, 41, 66, 12, 0};
